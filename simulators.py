66f05727e95f9f412251b19b285a5c4bf159099e
Leonardo Sagmeister <leomeister22@gmail.com>
2018-05-23 18:12:32 -0300

    Update simulators

---
+++ simulators.py
@@ -0,0 +1,18 @@
+import time
+import random
+from nameko.standalone.rpc import ClusterRpcProxy
+
+temperature = 20
+ph = 7.0
+humidity = 95.00
+config = {'AMQP_URI':'amqp://rabbit'}
+
+with ClusterRpcProxy(config) as cluster_rpc:
+    while True:
+        cluster_rpc.temperature_server.receive_temperature(temperature)
+        cluster_rpc.humidity_server.receive_humidity(humidity)
+        cluster_rpc.ph_server.receive_ph(ph)
+        time.sleep(2)
+        temperature = random.randint(23, 26) + random.random()
+        ph = round(random.uniform(6.6, 7.4), 1)
+        humidity = round(random.uniform(93.0, 97.9), 2)
