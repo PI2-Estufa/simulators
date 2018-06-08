80e71406d1333dbca305b4199415e8bbba32decf
Adailson Santos <adailson2@gmail.com>
2018-06-08 17:22:26 -0300

    Insert raw for water level simulator

--- simulators.py
+++ simulators.py
@@ -6,6 +6,7 @@ temperature = 20
 ph = 7.0
 humidity = 95.00
 ilumination = True
+water_level = 1
 config = {'AMQP_URI':'amqp://rabbit'}
 
 with ClusterRpcProxy(config) as cluster_rpc:
@@ -14,8 +15,11 @@ with ClusterRpcProxy(config) as cluster_rpc:
         cluster_rpc.humidity_server.receive_humidity(humidity)
         cluster_rpc.ph_server.receive_ph(ph)
         cluster_rpc.ilumination_server.receive_ilumination(ilumination)
+        cluster_rpc.water_level_server.receive_water_level(ilumination)
         time.sleep(2)
         temperature = random.uniform(23.0, 26.0)
         ph = random.uniform(6.6, 7.4)
         humidity = random.uniform(93.0, 97.9)
         ilumination = bool(random.getrandbits(1))
+        water_level = random.randint(1,2,3)
+
