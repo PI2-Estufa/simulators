b949e48d3e16fa1e93e5a2a1d2f43089a2778330
leomeister <leomeister22@gmail.com>
2018-06-08 16:21:08 -0300

    Add ilumination simulator to simulators.py

--- simulators.py
+++ simulators.py
@@ -5,6 +5,7 @@ from nameko.standalone.rpc import ClusterRpcProxy
 temperature = 20
 ph = 7.0
 humidity = 95.00
+ilumination = True
 config = {'AMQP_URI':'amqp://rabbit'}
 
 with ClusterRpcProxy(config) as cluster_rpc:
@@ -12,6 +13,7 @@ with ClusterRpcProxy(config) as cluster_rpc:
         cluster_rpc.temperature_server.receive_temperature(temperature)
         cluster_rpc.humidity_server.receive_humidity(humidity)
         cluster_rpc.ph_server.receive_ph(ph)
+        cluster_rpc.ph_server.receive_ilumination(ilumination)
         time.sleep(2)
         temperature = random.uniform(23.0, 26.0)
         ph = random.uniform(6.6, 7.4)
