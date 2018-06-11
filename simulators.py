d1f50c2d48c7123a52e40fabd3496b4f15a4c9b3
Adailson Santos <adailson2@gmail.com>
2018-06-11 19:17:17 -0300

    Add drawer status on simulators.py

--- simulators.py
+++ simulators.py
@@ -8,6 +8,8 @@ humidity = 95.00
 ilumination = True
 water_level = 1
 water_temperature = 20
+drawer_status = True
+
 config = {'AMQP_URI':'amqp://rabbit'}
 
 with ClusterRpcProxy(config) as cluster_rpc:
@@ -18,6 +20,7 @@ with ClusterRpcProxy(config) as cluster_rpc:
         cluster_rpc.ilumination_server.receive_ilumination(ilumination)
         cluster_rpc.water_level_server.receive_water_level(water_level)
         cluster_rpc.water_temperature_server.receive_water_temperature(water_temperature)
+        cluster_rpc.drawer_status_server.receive_drawer_status(drawer_status)
         time.sleep(2)
         temperature = random.uniform(23.0, 26.0)
         ph = random.uniform(6.6, 7.4)
@@ -25,4 +28,5 @@ with ClusterRpcProxy(config) as cluster_rpc:
         ilumination = bool(random.getrandbits(1))
         water_level = random.randint(0,2)
         water_temperature = random.uniform(23.0, 26.0)
+        drawer_status = bool(random.getrandbits(1))
 
